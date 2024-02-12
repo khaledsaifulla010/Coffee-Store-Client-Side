79943c221d8163e1e69313d722a3aaacf4234f17
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-02-12 23:26:57 +0600

    added toaster and added some functionalities to send data in server

--- src/Components/AddCoffee/AddCoffee.jsx
+++ src/Components/AddCoffee/AddCoffee.jsx
@@ -1,5 +1,5 @@
 
-
+import Swal from 'sweetalert2'
 const AddCoffee = () => {
     const handleAddCoffee = (event) => {
         event.preventDefault();
@@ -12,8 +12,30 @@ const AddCoffee = () => {
         const quantity = form.quantity.value
         const details = form.details.value;
 
-        const newCoffee = {name,supplier,category,photo,chef,quantity,details};
-        console.log(newCoffee)
+        const newCoffee = { name, supplier, category, photo, chef, quantity, details };
+        console.log(newCoffee);
+
+        //send data to the server
+
+        fetch('http://localhost:5000/coffee', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newCoffee)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    Swal.fire({
+                        title: 'Success',
+                        text: 'User Added Successfully!',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                    })
+                }
+            })
     }
     return (
         <div>
